b725dd9622bb3e99a66c612e7a1b29fd3bb518f9
karolyn1204 <dayita1204@gmail.com>
2016-06-13 15:09:57 -0500

    Update MPI_Scatterv.c

--- MPI_Scatterv.c
+++ MPI_Scatterv.c
@@ -1,37 +1,37 @@
-#include <mpi.h>
+#include "mpi.h"
 #include <stdio.h>
-#include <malloc.h>
-#define MAX 100
+#define SIZE 100
 
-int main(int argc, char** argv) {
-    	
-    int rank, size, i, acum, numtasks, rank;
-    int b[MAX], vector[MAX], disp[MAX], scounts[MAX];
-    
-    for (i = 1; i <= 100; i++)
-    	vector[ i - 1 ] = i;
-    	
-    MPI_Status stat;
-    MPI_Datatype rowtype;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+main(int argc, char *argv[])  {
+        int numtasks, rank, source=0, dest, tag=1, i;
+        int b[SIZE], acum, disp[SIZE], scounts[SIZE], a[SIZE];
+        
+        for (i = 1; i <= SIZE; i++)  a[ i - 1 ] = i;
+        
+        MPI_Status stat;
+        MPI_Datatype rowtype;
+        
+        MPI_Init(&argc,&argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+        
+        
+        for (i = 0; i < numtasks; i++) 
+        {
+                disp[i] = i*(SIZE/numtasks);
+                scounts[i] =(SIZE/numtasks);
+        }
 
-    for (i = 0; i < numtasks; i++) 
-    {
-      disp[i] = i*MAX;
-      scounts[i] = MAX;
-    }
-   
-    MPI_Scatterv(vector,scounts,disp,MPI_INT,b,MAX,MPI_INT,0,MPI_COMM_WORLD);
-    
-    acum = 0.0;
-    for (i = 0; i < MAX; i++)
-    	acum += b[i];		
-				
-    printf("Subtotal %lf en nodo %d\n", acum, rank);
-    MPI_Reduce(&acum,b,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-    if (rank == 0) printf("TOTAL: %lf\n",b[0]);
-   
-    MPI_Finalize();
+        MPI_Scatterv(a,scounts,disp,MPI_INT,b,SIZE/numtasks,MPI_INT,0,MPI_COMM_WORLD);
+        
+        acum = 0;
+        for (i = 0; i < SIZE/numtasks; i++)  acum += b[i];
+                
+        printf("Subtotal %d en nodo %d\n", acum, rank);
+        
+        MPI_Reduce(&acum,b,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+        
+        if (rank == 0) printf("TOTAL: %d\n",b[0]);
+        
+        MPI_Finalize();
 }
