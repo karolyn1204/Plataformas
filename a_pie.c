08751a44ed09e1df3f28a3f4ebe1f23c6b67ca05
karolyn1204 <dayita1204@gmail.com>
2016-06-13 16:23:09 -0500

    Update a_pie.c

--- a_pie.c
+++ a_pie.c
@@ -3,16 +3,15 @@
 #include <malloc.h>
 #include <time.h>
 
-#define MAX 100
 
 int main(int argc, char** argv) {
 
         float tt,inicio,final;
         inicio=clock();
-        
-        int vector[MAX], *miparte, stride;
+
         int rank, size, i, data, dest, source;
-        int tag = 1, acum = 0, total = 0;
+        int vector[MAX], *miparte, stride;
+        int tag = 1,  acum = 0,  total = 0;
         
         MPI_Status stat;
 
@@ -22,29 +21,31 @@ int main(int argc, char** argv) {
         MPI_Init(&argc,&argv);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        
         stride = MAX/(size - 1);
         
         if (rank == 0) 
         {
                 for (i = 1; i < size; i++)
-                        MPI_Send(&vector[stride*(i - 1)],stride,MPI_INT, i, tag$
-        } else {
+                        MPI_Send(&vector[stride*(i - 1)],stride,MPI_INT, i, tag, MPI_COMM_WORLD);
+        } else 
+        {
                 acum = 0;
                 miparte = (int*)malloc(sizeof(int)*stride);
-                MPI_Recv(miparte, stride, MPI_INT, 0, tag, MPI_COMM_WORLD, &sta$
+                MPI_Recv(miparte, stride, MPI_INT, 0, tag, MPI_COMM_WORLD, &stat);
                 for (i = 0, acum = 0; i < stride; i++)
                         acum += miparte[i];
-                }
-                
-        MPI_Reduce(&acum,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-  
-        if (rank == 0)
-        {
-          printf("Total: %d\n", total);
-          final= clock();
-          tt= (final-inicio)/(double) CLOCKS_PER_SEC;
-          printf("Tiempo transcurrido implementacion a pie: %f\n\n", tt);
         }
         
+        MPI_Reduce(&acum,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+        
+        if (rank == 0)
+        {
+                printf("Total: %d\n", total);
+                final= clock();
+                tt= (final-inicio)/(double) CLOCKS_PER_SEC;
+                printf("Tiempo transcurrido implementacion a pie: %f\n\n", tt);
+        }
+
         MPI_Finalize();
 }
