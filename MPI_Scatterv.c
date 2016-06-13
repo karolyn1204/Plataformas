a5be95d40936bf1f613c0ff648f18f0256ff1654
karolyn1204 <dayita1204@gmail.com>
2016-06-13 15:11:48 -0500

    Update MPI_Scatterv.c

--- MPI_Scatterv.c
+++ MPI_Scatterv.c
@@ -3,18 +3,16 @@
 #define SIZE 100
 
 main(int argc, char *argv[])  {
-        int numtasks, rank, source=0, dest, tag=1, i;
-        int b[SIZE], acum, disp[SIZE], scounts[SIZE], a[SIZE];
+        int numtasks, rank, i, acum, dest, source=0, tag=1;
+        int b[SIZE], disp[SIZE], scounts[SIZE], a[SIZE];
         
         for (i = 1; i <= SIZE; i++)  a[ i - 1 ] = i;
         
         MPI_Status stat;
         MPI_Datatype rowtype;
-        
         MPI_Init(&argc,&argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-        
         
         for (i = 0; i < numtasks; i++) 
         {
@@ -25,7 +23,7 @@ main(int argc, char *argv[])  {
         MPI_Scatterv(a,scounts,disp,MPI_INT,b,SIZE/numtasks,MPI_INT,0,MPI_COMM_WORLD);
         
         acum = 0;
-        for (i = 0; i < SIZE/numtasks; i++)  acum += b[i];
+        for (i = 0; i < (SIZE/numtasks); i++)  acum += b[i];
                 
         printf("Subtotal %d en nodo %d\n", acum, rank);
         
