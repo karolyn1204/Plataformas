f55e1974b078bbbdd83e6c2bcbd3b471ff0b5bdd
karolyn1204 <dayita1204@gmail.com>
2016-06-13 16:00:56 -0500

    Create a_pie.c

---
+++ a_pie.c
@@ -0,0 +1,50 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+
+#define MAX 100
+
+int main(int argc, char** argv) {
+
+float tt,inicio,final;
+inicio=clock();
+
+        int rank, size, i;
+        int vector[MAX], *miparte, stride;
+        int data;
+        int dest;
+        int source;
+        int tag = 1;
+        int acum = 0;
+        int total = 0;
+        MPI_Status stat;
+
+for (i = 1; i <= 100; i++)
+                vector[ i - 1 ] = i;
+        MPI_Init(&argc,&argv);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        stride = MAX/(size - 1);
+        if (rank == 0) {
+                for (i = 1; i < size; i++)
+                        MPI_Send(&vector[stride*(i - 1)],stride,MPI_INT, i, tag$
+        } else {
+                acum = 0;
+                miparte = (int*)malloc(sizeof(int)*stride);
+                MPI_Recv(miparte, stride, MPI_INT, 0, tag, MPI_COMM_WORLD, &sta$
+                for (i = 0, acum = 0; i < stride; i++)
+                        acum += miparte[i];
+        }
+        MPI_Reduce(&acum,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+  
+        if (rank == 0)
+        {
+          printf("Total: %d\n", total);
+          final= clock();
+          tt= (final-inicio)/(double) CLOCKS_PER_SEC;
+          printf("Tiempo transcurrido implementacion a pie: %f\n\n", tt);
+        }
+        
+        MPI_Finalize();
+}
