83414a748497d44a2c69e0ddf52d53738488d17f
karolyn1204 <dayita1204@gmail.com>
2016-06-13 20:02:47 -0500

    Update Riemann.c

--- Riemann.c
+++ Riemann.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
@@ -16,6 +15,7 @@ int main (int argc, char *argv[])
 {
       int rank, value, size, dest,i,workers;  
       float resultado,incremento,x,resultParcial;
+
       MPI_Status status; 
 
       incremento = ((float)XFINAL-(float)XINICIAL)/(float)PASOS;
@@ -29,50 +29,41 @@ int main (int argc, char *argv[])
 
       if (size<2)
       {
-    printf("No se puede procesar con %i nodos\n",size);
-    return 0;
+            printf("No se puede procesar con %i nodos\n",size);
+            return 0;
       }
 
       if (rank==MASTER)
       {
-    dest=1;
-    x=XINICIAL;
-    // Hacemos un Round Robin
-    for (i=0;i<PASOS;i++)
-    {
-      MPI_Send(&x, 1, MPI_FLOAT, dest, TAG_X, MPI_COMM_WORLD); 
-      dest++;
-      if (dest==size) dest=1;
-      x+=incremento;
-    }
-    for (i=MASTER+1;i<size;i++)
-    {
-      MPI_Recv(&resultParcial, 1, MPI_FLOAT, i, TAG_RESULT_PARCIAL, 
-                                          MPI_COMM_WORLD, &status); 
-      resultado +=resultParcial;
-    }
-
-    printf("El resultado de es %f \n",resultado);
-
- }
- else
- {
-    for (i=0; i<PASOS-(((int)(PASOS/workers))*workers)>=rank?
-                                          (int)(PASOS/workers)+1:
-                                          (int)(PASOS/workers); i++) 
-    {
-      MPI_Recv(&x, 1, MPI_FLOAT, MASTER, TAG_X, MPI_COMM_WORLD, 
-                                                          &status); 
-      resultParcial += incremento * (calcularFuncion(x)+
-                            calcularFuncion(x+incremento))/(float)2;
-
-    }
-    MPI_Send(&resultParcial,1, MPI_FLOAT, MASTER, TAG_RESULT_PARCIAL,
-                                                    MPI_COMM_WORLD);
+            dest=1;
+            x=XINICIAL;
+            for (i=0;i<PASOS;i++)
+            {
+                  MPI_Send(&x, 1, MPI_FLOAT, dest, TAG_X, MPI_COMM_WORLD); 
+                  dest++;
+                  if (dest==size) dest=1;
+                  x+=incremento;
+            }
+            
+            for (i=MASTER+1;i<size;i++)
+            {
+                  MPI_Recv(&resultParcial, 1, MPI_FLOAT, i, TAG_RESULT_PARCIAL, MPI_COMM_WORLD, &status); 
+                  resultado +=resultParcial;
+            }
+      printf("El resultado de es %f \n",resultado);
+            
+      }
+      
+      else
+      {
+            for (i=0; i<PASOS-(((int)(PASOS/workers))*workers)>=rank? (int)(PASOS/workers)+1: (int)(PASOS/workers); i++) 
+            {
+            MPI_Recv(&x, 1, MPI_FLOAT, MASTER, TAG_X, MPI_COMM_WORLD, &status); 
+            resultParcial += incremento * (calcularFuncion(x)+ calcularFuncion(x+incremento))/(float)2;
+                  
+            }
+      MPI_Send(&resultParcial,1, MPI_FLOAT, MASTER, TAG_RESULT_PARCIAL, MPI_COMM_WORLD);
       }      
 }
 
-float calcularFuncion(float x)
-{
-  return x*x;
-}
+float calcularFuncion(float x) {  return x*x; }
