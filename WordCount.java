62e502991a9d962865910ed9adad6747d6711feb
karolyn1204 <dayita1204@gmail.com>
2016-06-27 18:36:21 -0500

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -1,4 +1,4 @@
-  import java.io.IOException;
+import java.io.IOException;
 import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -13,45 +13,41 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCount {
 
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       StringTokenizer itr = new StringTokenizer(value.toString(),"\t");
-
-        word.set(itr.nextToken());
-        context.write(word, one);
-
+      word.set(itr.nextToken());
+      context.write(word, one);
     }
   }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
     Text maxWord=new Text();
     private int max=0;
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
       int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
+      for (IntWritable val : values) {  sum += val.get();      }
 
       if(sum>max)
       {
       max=sum;
       maxWord.set(key);
-       }
-}
-protected void cleanup(Context context) throws IOException, InterruptedException{
+      }
+    }
+
+    protected void cleanup(Context context) throws IOException, InterruptedException{
     context.write(maxWord, new IntWritable(max));
-}
-}
+    }
+  }
+
+
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
@@ -66,4 +62,5 @@ protected void cleanup(Context context) throws IOException, InterruptedException
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
+  
 }
